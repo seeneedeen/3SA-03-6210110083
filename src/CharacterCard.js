da7ebe17a84000dfe9bbec29fda8d24eed6044de
seeneedeen <6210110083@psu.ac.th>
2021-07-05 13:38:15 +0700

    first commit

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import { at } from 'lodash';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function CharacterCard(props) {
     const [active, setActive] = useState(false);
+    const attemptRef = useRef(props.attempt);
 
     const activate = () => {
     setActive(true)
     props.activationHandler(props.value)
     }
 
- const className = `card ${active ? 'activeCard': ''}`
+    useEffect(()=>{
+        if(attemptRef.current != props.attempt){
+            setActive(false);
+            attemptRef.current = props.attempt
+        }
+    })
+    
+    const className = `card ${active ? 'activeCard': ''}`
 
     return (
         <div className={className} onClick={activate}>{props.value}</div>
