abdd67ba514320b6fe8ca008cf7f621ce6af8b6a
seeneedeen <6210110083@psu.ac.th>
2021-07-05 11:16:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import CharacterCard from './CharacterCard';
+const word = "Hello";
 function App() {
  return (
  <div>
- <CharacterCard value="h"/>
- <CharacterCard value="i"/>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
  </div>
  );
 }
+
 export default App;
